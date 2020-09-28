4ac6c9ad4956d4cebfa8aace69324a8256ad28dc
hsaud <hbensaud@gmail.com>
2020-09-28 10:30:18 -0400

    Add test to test the real ranking service

--- SWE637-HW3/src/swe637/hw3/ReservationServiceTest.java
+++ SWE637-HW3/src/swe637/hw3/ReservationServiceTest.java
@@ -7,31 +7,27 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
 
 public class ReservationServiceTest {
 
 	private ReservationService reservationService = null;
 	private Customer customer = null;
 	private RankingService rankingService = null;
+	private RankingService realRankingService = null;
 
 	@Before
 	public void setUp() {
 		reservationService = new ReservationService();
 		customer = new Customer("John Smith", new Rank(10));
 		rankingService = mock(RankingService.class); //creates mock
-	}
-
-	
-	@After
-	public void tearDown() throws Exception {
+		realRankingService = new RankingService();
 	}
 
 	
 	@Test
 	public void testReservationService() {
-		reservationService.reserve(customer);
-		//how to assert reservation is successful?
+		ReservationService spyService = spy(reservationService);
+		doNothing().when(spyService).reserve(customer);	
 	}
 
 	@Test
@@ -41,5 +37,23 @@ public class ReservationServiceTest {
 		when(rankingService.getRank(customer)).thenReturn(rank);
 		assertEquals(rankingService.getRank(customer), rank);
 	}
+	
+	@Test
+	public void testRealRankingService() {
+		Rank rank = new Rank(2);
+		customer.setRank(rank);
+		when(realRankingService.getRank(customer)).thenReturn(rank);
+		assertEquals(realRankingService.getRank(customer), rank);
+	}
+	
+	@Test
+	public void testRankingServiceFail() {
+		Rank rank = new Rank(2);
+		Rank anotherRank = new Rank(10);
+		customer.setRank(rank);
+		when(rankingService.getRank(customer)).thenReturn(rank);
+		assertNotEquals(rankingService.getRank(customer), rank);
+	}
 
+	
 }
